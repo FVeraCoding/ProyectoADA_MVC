99b0cc9bc093116330887360bfde33af632d8c06
FVeraCoding <fveracoding@gmail.com>
2024-10-25 09:38:16 +0200

    Inicialización de proyecto con Maven.

---
+++ src/main/java/Modelo/Clases/Concesionario.java
@@ -0,0 +1,14 @@
+
+package Modelo.Clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Concesionario {
+    private String NOMBRE;
+    private String CEO;
+    private List<Sucursal> listaSucursales = new ArrayList<>();
+    
+    
+}
