a7552004e6c4119a31e85b9b891a28c6ade63a7c
FVeraCoding <fveracoding@gmail.com>
2024-10-25 10:08:22 +0200

    Clase Concesionario y Clase Sucursal

--- src/main/java/Modelo/Clases/Concesionario.java
+++ src/main/java/Modelo/Clases/Concesionario.java
@@ -3,12 +3,45 @@ package Modelo.Clases;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 
-
+@XmlRootElement
 public class Concesionario {
-    private String NOMBRE;
+   
+    private String nombre;
     private String CEO;
     private List<Sucursal> listaSucursales = new ArrayList<>();
-    
-    
+
+    public Concesionario() {
+    }
+
+    @XmlElement
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlElement
+    public String getCEO() {
+        return CEO;
+    }
+
+    public void setCEO(String CEO) {
+        this.CEO = CEO;
+    }
+
+    @XmlElementWrapper(name = "sucursales")
+    @XmlElement(name = "sucursal")
+    public List<Sucursal> getListaSucursales() {
+        return listaSucursales;
+    }
+
+    public void setListaSucursales(List<Sucursal> listaSucursales) {
+        this.listaSucursales = listaSucursales;
+    }    
 }
