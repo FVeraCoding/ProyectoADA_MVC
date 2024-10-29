b50056a83f1e1f58e1ce83a73b9f8966dfd4e151
ArturSchuldt <artur.schuldt.iturri.al@iespoligonosur.org>
2024-10-29 15:33:42 +0100

    Clases Empleado Coche

--- src/main/java/Modelo/Clases/Coche.java
+++ src/main/java/Modelo/Clases/Coche.java
@@ -4,10 +4,96 @@
  */
 package Modelo.Clases;
 
+import java.util.Date;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
 /**
  *
  * @author alumnadotarde
  */
+@XmlType(propOrder = { "id", "marca", "modelo", "fchFabricacion", "precio", "color", "numPuertas", "combustible" })
 public class Coche {
-    
+
+    private int id;
+    private String marca;
+    private String modelo;
+    private Date fchFabricacion;
+    private double precio;
+    private String color;
+    private int numPuertas;
+    private String combustible;
+
+    @XmlAttribute
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @XmlElement
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @XmlElement
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @XmlElement(name = "creacion")
+    public Date getFchFabricacion() {
+        return fchFabricacion;
+    }
+
+    public void setFchFabricacion(Date fchFabricacion) {
+        this.fchFabricacion = fchFabricacion;
+    }
+
+    @XmlElement
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @XmlElement
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @XmlElement(name = "num_puertas")
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    @XmlElement
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
 }
--- src/main/java/Modelo/Clases/Empleado.java
+++ src/main/java/Modelo/Clases/Empleado.java
@@ -4,17 +4,102 @@
  */
 package Modelo.Clases;
 
-/**
- *
- * @author alumnadotarde
- */
+import java.util.Date;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "id", "nombre", "apellidos", "dni", "puesto", "telefono", "correo", "sueldo", "fechaContrato" })
 public class Empleado {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String puesto;
+    private String telefono;
+    private String correo;
+    private double sueldo;
+    private Date fechaContrato;
+
+    @XmlAttribute
+    public int getId() {
+        return id;
     }
-    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @XmlElement
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlElement
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @XmlElement
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @XmlElement
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    @XmlElement
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @XmlElement
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @XmlElement
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @XmlElement(name = "fecha_contrato")
+    public Date getFechaContrato() {
+        return fechaContrato;
+    }
+
+    public void setFechaContrato(Date fechaContrato) {
+        this.fechaContrato = fechaContrato;
+    }
 }
