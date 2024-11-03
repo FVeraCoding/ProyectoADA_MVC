32c68e3505633db1797fc797040f451cf9ac477b
ArturSchuldt <artur.schuldt.iturri.al@iespoligonosur.org>
2024-11-03 17:04:40 +0100

    Interfaz

--- src/main/java/Modelo/Clases/Concesionario.java
+++ src/main/java/Modelo/Clases/Concesionario.java
@@ -1,9 +1,9 @@
-
+    
 package Modelo.Clases;
 
 import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlElementWrapper;         
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "concesionario")
